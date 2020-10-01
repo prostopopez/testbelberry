f96d9c215ab3f48eda1bee298d799d9b9be40fd0
ilyashishov <stalk1258@gmail.com>
2020-10-01 03:28:08 +0300

    #FLACON-2 Создать layout

---
+++ gulpfile.js
@@ -0,0 +1,104 @@
+const gulp = require(`gulp`);
+const sass = require(`gulp-sass`);
+const autoprefixer = require(`gulp-autoprefixer`);
+const cleanCSS = require(`gulp-clean-css`);
+const browserSync = require(`browser-sync`).create();
+const twig = require(`gulp-twig`);
+const babel = require(`gulp-babel`);
+const uglify = require(`gulp-uglify`);
+const sourcemaps = require(`gulp-sourcemaps`);
+const concat = require(`gulp-concat`);
+const cssClassPrefix = require(`gulp-css-class-prefix`);
+const htmlClassPrefix = require(`gulp-html-prefix`);
+const svgSprite = require(`gulp-svg-sprite`);
+
+const getPublicFolderPath = (ext = ``) => gulp.dest(`./public/${ext}`);
+const classPrefix = `fnv-`;
+
+gulp.task('svgSprite', function () {
+    return gulp.src('img/svg/*.svg')
+        .pipe(svgSprite({
+                mode: {
+                    stack: {
+                        sprite: '../sprite.svg'
+                    }
+                },
+            }
+        ))
+        .pipe(gulp.dest(getPublicFolderPath(`img`)));
+});
+
+gulp.task(`html`, gulp.series(function () {
+    return gulp.src(`./html/pages/**/*.html`)
+        .pipe(twig({
+            data: {}
+        }))
+        .pipe(htmlClassPrefix(classPrefix))
+        .pipe(getPublicFolderPath())
+        .pipe(browserSync.stream());
+}));
+
+gulp.task(`img`, gulp.series(function () {
+    return gulp
+        .src(`./img/**/*`, { base: `.` })
+        .pipe(getPublicFolderPath());
+}));
+
+gulp.task(`js`, gulp.series(function () {
+    return gulp.src(`js/[^(_)]*.js`)
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: [`@babel/env`]
+        }))
+        .pipe(concat(`main.mini.js`))
+        .pipe(uglify())
+        .pipe(sourcemaps.write(`.`))
+        .pipe(getPublicFolderPath(`js`))
+        .pipe(browserSync.stream());
+}));
+
+gulp.task(`jsForPage`, gulp.series(function () {
+    return gulp.src(`js/_*.js`)
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: [`@babel/env`]
+        }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write(`.`))
+        .pipe(getPublicFolderPath(`js`))
+        .pipe(browserSync.stream());
+}));
+
+gulp.task(`styles`, gulp.series(function () {
+    return gulp.src(`sass/*.scss`)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on(`error`, sass.logError))
+        .pipe(cssClassPrefix(classPrefix, { ignored: [/\.ui-/] }))
+        .pipe(cleanCSS({ compatibility: ` 'ie8'` }))
+        .pipe(autoprefixer({
+            browsers: [`last 2 versions`],
+            cascade: false,
+            grid: `no-autoplace`
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(getPublicFolderPath(`css`))
+        .pipe(browserSync.stream());
+}));
+
+gulp.task(`default`, gulp.parallel(`styles`, `html`, `img`, `js`, `jsForPage`, function () {
+    gulp.watch(`sass/**/*.scss`, gulp.parallel(`styles`));
+    gulp.watch(`html/**/*`).on('change', gulp.parallel(`html`));
+    gulp.watch(`img/*`).on('change', gulp.parallel(`img`));
+    gulp.watch(`js/*`).on('change', gulp.parallel(`js`));
+    gulp.watch(`js/*`).on('change', gulp.parallel(`jsForPage`));
+    gulp.watch(`public/*`).on('change', browserSync.reload);
+}));
+
+gulp.task(`build`, gulp.parallel(`styles`, `html`, `img`, `js`, `jsForPage`));
+
+gulp.task(`sync`, gulp.parallel(`default`, function () {
+    browserSync.init({
+        server: `./public`,
+        notify: false
+    });
+}));
