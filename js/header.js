dfa250b6dc0cc61d94298e4bcf33baa77601f6c5
Gleb Kupriyanko <glebkupriyanko@Mac-mini-Kirill.local>
2020-10-06 13:41:51 +0300

    #FLACON-3 dropdown ready

--- js/header.js
+++ js/header.js
@@ -1,24 +1,17 @@
-//Dropdown on Catalog Pages
-const filterDropdown = document.querySelectorAll(`.fnv-dropdown`);
+//user dropdown on header
+const userDropdown = document.querySelector(`.fnv-userDropdown`);
+const newDrop = userDropdown.querySelector(`.fnv-newDrop`);
+let active = false;
 
-for (let i = 0; i < filterDropdown.length; i++) {
-    let filterTrigger = filterDropdown[i].querySelector(`.fnv-filterName`);
-    let filterContent = filterDropdown[i].querySelector(`.fnv-dropdown-content`);
+newDrop.addEventListener('click', function (event) {
+    active = !active;
+    toggleDropdown(active, userDropdown);
+});
 
-    filterTrigger.addEventListener('click', function (event) {
-        if (filterTrigger.contains(filterTrigger)) {
-            let toOpen = filterContent.classList.contains('fnv-closeDrop');
-            toggleFilterButton(toOpen, filterContent, filterTrigger);
-        }
-    });
-}
-
-function toggleFilterButton(toOpen, filterContent, filterTrigger) {
-    if (toOpen) {
-        filterContent.classList.remove(`fnv-closeDrop`);
-        filterTrigger.classList.remove(`fnv-whileClosed`);
+function toggleDropdown(active, userDropdown) {
+    if (active) {
+        userDropdown.classList.add(`fnv-active`);
     } else {
-        filterContent.classList.add(`fnv-closeDrop`);
-        filterTrigger.classList.add(`fnv-whileClosed`);
+        userDropdown.classList.remove(`fnv-active`);
     }
 }
