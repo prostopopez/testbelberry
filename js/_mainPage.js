b0262b376457f6a82ac9ccad0b47bbceab1bc0e7
prostopopez <gtaas@mail.ru>
2021-06-19 08:32:45 +0300

    changes

---
+++ js/_mainPage.js
@@ -0,0 +1,73 @@
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.querySelectorAll(".fnv-slider .fnv-slide");
+    console.log(slides);
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+}
+
+// stock slider
+let slider = tns({
+    container: '.fnv-stockSlider',
+    loop: true,
+    controls: false,
+    autoplayButtonOutput: false,
+    nav: false,
+    items: 3,
+    slideBy: 'page',
+    gutter: 30,
+    speed: 1500,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    swipeAngle: false
+});
+
+let slider2 = tns({
+    container: '.fnv-gallerySlider1',
+    axis: "vertical",
+    loop: true,
+    controls: false,
+    autoplayButtonOutput: false,
+    nav: false,
+    items: 1,
+    gutter: 30,
+    speed: 2000,
+    autoplay: true,
+    autoplayTimeout: 3500,
+    swipeAngle: false
+});
+
+let slider3 = tns({
+    container: '.fnv-gallerySlider2',
+    loop: true,
+    controls: false,
+    autoplayButtonOutput: false,
+    nav: false,
+    items: 1,
+    gutter: 30,
+    speed: 2000,
+    autoplay: true,
+    autoplayTimeout: 3500,
+    swipeAngle: false
+});
