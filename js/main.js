25b6aefa1c20605b449eb188bcf29fa118ffa234
prostopopez <gtaas@mail.ru>
2021-06-22 13:55:25 +0300

    radio

--- js/main.js
+++ js/main.js
@@ -23,3 +23,28 @@ for (let i = 0; i < countDropdowns.length; i++) {
         });
     }
 }
+
+let dropdowns = document.querySelectorAll('.fnv-dropdowns .fnv-dropdown');
+
+for (let i = 0; i < dropdowns.length; i++) {
+    if (!!dropdowns[i].querySelectorAll('.fnv-dropdownItem')) {
+        let items = dropdowns[i].querySelectorAll('.fnv-dropdownItem');
+
+        if (!!dropdowns[i].querySelectorAll('.fnv-dropdownItem .fnv-choiceCheck button')) {
+            let allChoices = dropdowns[i].querySelectorAll('.fnv-dropdownItem .fnv-choiceCheck button');
+            console.log(allChoices);
+
+            for (let j = 0; j < items.length; j++) {
+                let choice = items[j].querySelector('.fnv-choiceCheck button');
+
+                choice.addEventListener('click', function () {
+                    for (let f = 0; f < allChoices.length; f++) {
+                        allChoices[f].classList.remove(`fnv-active`);
+                    }
+
+                    choice.classList.add(`fnv-active`);
+                });
+            }
+        }
+    }
+}
