3290ac82b913289331d24f7d7b2ceed7b0f36680
prostopopez <gtaas@mail.ru>
2021-06-22 17:58:27 +0300

    priceFix

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+let prices = {
+    ssd: 11500,
+    cpu: 21000,
+    gpu: 101000,
+    ram: 4700,
+    motherboard: 14000,
+    hdd: 2900,
+    cooler: 4500,
+    bp: 7400
+};
+
+let ssdCount;
+let gpuCount;
+let ramCount;
+let hddCount;
+let finalPrice;
+
+function changeFinal() {
+    ssdCount = document.querySelector('.fnv-ssd .fnv-detailCount').value;
+    gpuCount = document.querySelector('.fnv-gpu .fnv-detailCount').value;
+    ramCount = document.querySelector('.fnv-ram .fnv-detailCount').value;
+    hddCount = document.querySelector('.fnv-hdd .fnv-detailCount').value;
+
+    finalPrice = prices.ssd * ssdCount + prices.cpu + prices.gpu * gpuCount + prices.ram * ramCount + prices.motherboard + prices.hdd * hddCount + prices.cooler + prices.bp;
+
+    document.querySelector(`.fnv-finalPrice`).innerHTML = finalPrice + `₽`;
+}
+
+window.onload = function () {
+    changeFinal();
+};
+
+/*------*/
 let countDropdowns = document.querySelectorAll('.fnv-dropdowns .fnv-dropdown .fnv-plusMinus');
 
 for (let i = 0; i < countDropdowns.length; i++) {
@@ -11,6 +44,8 @@ for (let i = 0; i < countDropdowns.length; i++) {
                 count--;
                 countDropdowns[i].querySelector('.fnv-detailCount').value = count;
             }
+
+            changeFinal();
         });
     }
 
@@ -20,10 +55,13 @@ for (let i = 0; i < countDropdowns.length; i++) {
         plus.addEventListener('click', function () {
             count++;
             countDropdowns[i].querySelector('.fnv-detailCount').value = count;
+
+            changeFinal();
         });
     }
 }
 
+/*------*/
 let dropdowns = document.querySelectorAll('.fnv-dropdowns .fnv-dropdown');
 
 for (let i = 0; i < dropdowns.length; i++) {
@@ -32,17 +70,25 @@ for (let i = 0; i < dropdowns.length; i++) {
 
         if (!!dropdowns[i].querySelectorAll('.fnv-dropdownItem .fnv-choiceCheck button')) {
             let allChoices = dropdowns[i].querySelectorAll('.fnv-dropdownItem .fnv-choiceCheck button');
-            console.log(allChoices);
 
             for (let j = 0; j < items.length; j++) {
-                let choice = items[j].querySelector('.fnv-choiceCheck button');
+                let choice = items[j].querySelector('.fnv-choiceCheck');
 
-                choice.addEventListener('click', function () {
+                choice.querySelector('button').addEventListener('click', function () {
                     for (let f = 0; f < allChoices.length; f++) {
                         allChoices[f].classList.remove(`fnv-active`);
                     }
 
-                    choice.classList.add(`fnv-active`);
+                    dropdowns[i].querySelector('.fnv-detailShort').innerHTML = choice.querySelector('p').innerHTML;
+                    choice.querySelector('button').classList.add(`fnv-active`);
+
+                    if (j === 0) {
+                        prices.ssd = 11500;
+                    } else {
+                        prices.ssd = 19603;
+                    }
+
+                    changeFinal();
                 });
             }
         }
