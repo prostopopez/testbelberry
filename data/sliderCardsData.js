3ec1bc74151ff58c0ee7e3c424c3e83840ff7167
prostopopez <gtaas@mail.ru>
2022-03-26 14:45:15 +0300

    cardsLookingGood

--- data/sliderCardsData.js
+++ data/sliderCardsData.js
@@ -2,46 +2,46 @@ module.exports = [
     {
         type: 'Акция',
         name: 'Он-лайн консультация любого врача-терапевта',
-        oldPrice: '1 700 руб.',
-        discount: '30%',
-        newPrice: '1 190 руб.',
-        upTo: '30.09.2021 г.',
+        oldPrice: '1 700',
+        discount: '30',
+        newPrice: '1 190',
+        upTo: '30.09.2021',
         img: '../img/discount/doctorMale.png'
     },
     {
         type: 'Акция',
         name: 'Расшифровка анализов у любого доктора',
-        oldPrice: '800 руб.',
-        discount: '30%',
-        newPrice: '500 руб.',
-        upTo: '30.09.2021 г.',
+        oldPrice: '800',
+        discount: '30',
+        newPrice: '500',
+        upTo: '30.09.2021',
         img: '../img/discount/doctorFemale.png'
     },
     {
         type: 'Акция',
-        name: 'ЭКГ + Эхо-кг',
-        oldPrice: '4 150 руб.',
-        discount: '30%',
-        newPrice: '2 900 руб.',
-        upTo: '30.09.2021 г.',
+        name: 'ЭКГ + ЭХО-КГ',
+        oldPrice: '4 150',
+        discount: '30',
+        newPrice: '2 900',
+        upTo: '30.09.2021',
         img: '../img/discount/medDevice.png'
     },
     {
         type: 'Акция',
         name: 'УЗИ',
-        oldPrice: '1 700 руб.',
-        discount: '30%',
-        newPrice: '1 190 руб.',
-        upTo: '30.09.2021 г.',
+        oldPrice: '1 700',
+        discount: '30',
+        newPrice: '1 190',
+        upTo: '30.09.2021',
         img: '../img/discount/behindMachine.png'
     },
     {
         type: 'Акция',
         name: 'Прием любого врача',
-        oldPrice: '1 700 руб.',
-        discount: '30%',
-        newPrice: '1 190 руб.',
-        upTo: '30.09.2021 г.',
+        oldPrice: '1 700',
+        discount: '30',
+        newPrice: '1 190',
+        upTo: '30.09.2021',
         img: '../img/discount/doctorSmile.png'
     }
 ];
